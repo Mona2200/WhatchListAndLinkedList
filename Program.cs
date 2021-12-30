b52d01709475691a9c9a3faef583cf1fe4875e2f
Алёна Мелехова <alenamelehova13@gmail.com>
2021-12-30 14:16:11 +0300

    Готовая программа

--- Program.cs
+++ Program.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿// <copyright file="Program.cs" company="Alyona">
+// Copyright (c) 2012 All Rights Reserved
+// </copyright>
 
-namespace WhatchListAndLinkedList
+namespace Programm5
 {
-   class Program
+   using System;
+   using System.Collections.Generic;
+   using System.Diagnostics;
+   using System.IO;
+
+   /// <summary>
+   /// Program Class. 
+   /// </summary>
+   public class Program
    {
-      static void Main(string[] args)
+      /// <summary>
+      /// Main Method.
+      /// </summary>
+      public static void Main()
       {
-         Console.WriteLine("Hello World!");
+         var words = ReadWords();
+         var timelist = WatchList(words);
+         var timelinkedlist = WatchLinkedList(words);
+         Console.WriteLine(timelist.ToString() + " мс");
+         Console.WriteLine(timelinkedlist.ToString() + " мс");
+      }
+
+      /// <summary>
+      /// Reading the file.
+      /// </summary>
+      /// <returns>Array of words from the text.</returns>
+      public static string[] ReadWords()
+      {
+         string text = File.ReadAllText("C:/Text1.txt");
+         char[] delimiters = new char[] { ' ', '\r', '\n' };
+         var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+         return words;
+      }
+
+      /// <summary>
+      /// Using List.
+      /// </summary>
+      ///  /// <param name="words">Array of words.</param>
+      /// <returns>Program runtime.</returns>
+      public static double WatchList(string[] words)
+      {
+         var watch = Stopwatch.StartNew();
+         var list = new List<string>();
+         foreach (var word in words)
+         {
+            list.Add(word);
+         }
+
+         return watch.Elapsed.TotalMilliseconds;
+      }
+
+      /// <summary>
+      /// Using LinkedList.
+      /// </summary>
+      ///  /// <param name="words">Array of words.</param>
+      /// <returns>Program runtime.</returns>
+      public static double WatchLinkedList(string[] words)
+      {
+         var watch = Stopwatch.StartNew();
+         var list = new LinkedList<string>();
+         foreach (var word in words)
+         {
+            list.AddFirst(word);
+         }
+
+         return watch.Elapsed.TotalMilliseconds;
       }
    }
 }
